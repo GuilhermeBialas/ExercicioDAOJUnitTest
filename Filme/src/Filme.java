efca78654900343c8ca9b460f731ba1a4abfb2a4
GuilhermeBialas <guilhermebialas31@gmail.com>
2018-10-08 11:07:17 -0300

    inicio

---
+++ Filme/src/Filme.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alunos
+ */
+public class Filme {
+     private String nome, diretor, categoria, atorPrincipal, faixaEtaria, idiomaOriginal; 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getAtorPrincipal() {
+        return atorPrincipal;
+    }
+
+    public void setAtorPrincipal(String atorPrincipal) {
+        this.atorPrincipal = atorPrincipal;
+    }
+
+    public String getFaixaEtaria() {
+        return faixaEtaria;
+    }
+
+    public void setFaixaEtaria(String faixaEtaria) {
+        this.faixaEtaria = faixaEtaria;
+    }
+
+    public String getIdiomaOriginal() {
+        return idiomaOriginal;
+    }
+
+    public void setIdiomaOriginal(String idiomaOriginal) {
+        this.idiomaOriginal = idiomaOriginal;
+    }
+
+    public boolean isLegenda() {
+        return legenda;
+    }
+
+    public void setLegenda(boolean legenda) {
+        this.legenda = legenda;
+    }
+
+    public boolean isDublado() {
+        return dublado;
+    }
+
+    public void setDublado(boolean dublado) {
+        this.dublado = dublado;
+    }
+
+    public short getTempoFilme() {
+        return tempoFilme;
+    }
+
+    public void setTempoFilme(short tempoFilme) {
+        this.tempoFilme = tempoFilme;
+    }
+
+    public double getFaturamento() {
+        return faturamento;
+    }
+
+    public void setFaturamento(double faturamento) {
+        this.faturamento = faturamento;
+    }
+
+    public double getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(double orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public short getAno() {
+        return ano;
+    }
+
+    public void setAno(short ano) {
+        this.ano = ano;
+    }
+    private boolean legenda, dublado;
+    private short tempoFilme;
+    private double faturamento, orcamento;
+    private short ano;
+}
