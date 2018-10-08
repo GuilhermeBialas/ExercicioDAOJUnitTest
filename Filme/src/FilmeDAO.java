42c0ba080ae8f7e80d369d864e16d84acfb49c36
GuilhermeBialas <guilhermebialas31@gmail.com>
2018-10-08 11:14:43 -0300

    inicio

---
+++ Filme/src/FilmeDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Guilheme Henrique Bialas
+ * @Date 08/10/2018
+ */
+public class FilmeDAO {
+    int cadastrar(Filme filme){
+        Connection conexao = Conexao.conectar();
+        if (conexao != null) {
+            String sql = "INSERT INTO filmes "
+                    +"\n"
+            
+        }
+    }
+    
+}
