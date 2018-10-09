e39e030578471fd63bbea96e4a90306e91277c1b
Alunos <gersonsbeltrame@gmail.com>
2018-10-09 08:14:02 -0300

    arrumei o estrutura

--- Filme/test/FilmeDAOJUnitTest.java
+++ Filme/test/FilmeDAOJUnitTest.java
@@ -27,8 +27,8 @@ public class FilmeDAOJUnitTest {
         sony.setIdiomaOriginal("Inglês");
         sony.setLegenda(true);
         sony.setOrcamento(500000);
-        sony.setTempoFilme();
-        
+        sony.setTempoFilme(150);
+        sony.setAno();
     }
     
     
